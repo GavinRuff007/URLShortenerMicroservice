3782bf709592ce6b70312f38f67c302818c5f9c6
Parsa Eftekharmanesh <134914088+GavinRuff007@users.noreply.github.com>
2025-04-18 09:37:18 +0330

    Initial commit and Design Spec of MonoRepo Project

---
+++ expiration-service/src/main/java/org/example/expirationservice/ExpirationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.expirationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpirationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpirationServiceApplication.class, args);
+    }
+
+}
---
+++ expiration-service/src/test/java/org/example/expirationservice/ExpirationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.expirationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpirationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
