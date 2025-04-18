c51361accd3af9d523fee9940f75f2e686def617
Parsa Eftekharmanesh <134914088+GavinRuff007@users.noreply.github.com>
2025-04-18 18:17:55 +0330

    create caching module for storing data in Redis

---
+++ caching-service/src/test/java/org/example/cachingservice/CachingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cachingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
