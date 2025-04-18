0ec5139491715ba3b25c2f39f857255053b05442
Parsa Eftekharmanesh <134914088+GavinRuff007@users.noreply.github.com>
2025-04-19 03:19:54 +0330

    refactor: delete useless module for redirectservice branch

--- api-gateway/src/main/java/org/example/apigateway/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.apigateway.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
--- api-gateway/src/main/java/org/example/apigateway/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.apigateway.repository;
-
-import org.example.apigateway.model.UserEntity;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
-
-public interface UserRepository extends ReactiveCrudRepository<UserEntity, Long> {
-    Mono<UserEntity> findByUsername(String username);
-}
